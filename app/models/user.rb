dec4c20350025956486302b4339287392bd65baf
Ben Matthews <ben@montfort.io>
2017-12-01 21:08:44 +0000

    rails_apps_composer: add README files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,4 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :vip, :admin]
-  after_initialize :set_default_role, :if => :new_record?
-
-  def set_default_role
-    self.role ||= :user
-  end
-
   enum role: [:user, :admin, :silver, :gold, :platinum]
   after_initialize :set_default_role, :if => :new_record?
   after_initialize :set_default_plan, :if => :new_record?
